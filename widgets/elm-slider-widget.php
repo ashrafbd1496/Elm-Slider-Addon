c916465b9aea99806c4c691df0a266626cffe801
ashrafbd1496 <asraf1496@gmail.com>
2023-08-07 18:32:57 +0600

    added slide attributes

--- widgets/elm-slider-widget.php
+++ widgets/elm-slider-widget.php
@@ -53,53 +53,83 @@ class Elm_Slider_Widget extends \Elementor\Widget_Base {
 		$repeater = new \Elementor\Repeater();
 
 		$repeater->add_control(
-			'list_title',
+			'slide_title',
 			[
 				'label' => esc_html__( 'Title', 'elm-slider' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'List Title' , 'elm-slider' ),
+				'default' => esc_html__( 'Slide Title' , 'elm-slider' ),
+				'label_block' => true,
+			]
+		);
+		$repeater->add_control(
+			'slide_subtitle',
+			[
+				'label' => esc_html__( 'Sub Title', 'elm-slider' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Slide Sub Title' , 'elm-slider' ),
 				'label_block' => true,
 			]
 		);
 
 		$repeater->add_control(
-			'list_content',
+			'photo',
 			[
-				'label' => esc_html__( 'Content', 'elm-slider' ),
-				'type' => \Elementor\Controls_Manager::WYSIWYG,
-				'default' => esc_html__( 'List Content' , 'elm-slider' ),
-				'show_label' => false,
+				'label' => esc_html__( 'Photo', 'elm-slider' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'label_block' => true,
 			]
 		);
 
 		$repeater->add_control(
-			'list_color',
+			'btn_text_1',
 			[
-				'label' => esc_html__( 'Color', 'elm-slider' ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} {{CURRENT_ITEM}}' => 'color: {{VALUE}}'
-				],
+				'label' => esc_html__( 'Button Text 1', 'elm-slider' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Learn More' , 'elm-slider' ),
+				'show_label' => false,
+			]
+		);
+		$repeater->add_control(
+			'btn_url_1',
+			[
+				'label' => esc_html__( 'Button Link 1', 'elm-slider' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'show_label' => false,
+			]
+		);
+		$repeater->add_control(
+			'btn_text_2',
+			[
+				'label' => esc_html__( 'Button Text 2', 'elm-slider' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Read More' , 'elm-slider' ),
+				'show_label' => false,
+			]
+		);
+		$repeater->add_control(
+			'btn_url_2',
+			[
+				'label' => esc_html__( 'Button Link 2', 'elm-slider' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'show_label' => false,
 			]
 		);
 
 		$this->add_control(
-			'list',
+			'slides',
 			[
-				'label' => esc_html__( 'Repeater List', 'elm-slider' ),
+				'label' => esc_html__( 'Slides', 'elm-slider' ),
 				'type' => \Elementor\Controls_Manager::REPEATER,
 				'fields' => $repeater->get_controls(),
 				'default' => [
 					[
-						'list_title' => esc_html__( 'Title #1', 'elm-slider' ),
-						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'elm-slider' ),
+						'slide_title' => esc_html__( 'Slide Title', 'elm-slider' ),
+						'slide_subtitle' => esc_html__( 'Slide Sub Titile.', 'elm-slider' ),
+						'btn_text_1' => esc_html__( 'Button Text.', 'elm-slider' ),
 					],
-					[
-						'list_title' => esc_html__( 'Title #2', 'elm-slider' ),
-						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'elm-slider' ),
-					],
+					
 				],
-				'title_field' => '{{{ list_title }}}',
+				'title_field' => '{{{ slide_title }}}',
 			]
 		);
 
@@ -121,11 +151,15 @@ class Elm_Slider_Widget extends \Elementor\Widget_Base {
 
 		$settings = $this->get_settings_for_display();
 
-		if ( $settings['list'] ) {
+		if ( $settings['slides'] ) {
 			echo '<dl>';
-			foreach (  $settings['list'] as $item ) {
-				echo '<dt class="elementor-repeater-item-' . esc_attr( $item['_id'] ) . '">' . $item['list_title'] . '</dt>';
-				echo '<dd>' . $item['list_content'] . '</dd>';
+			foreach (  $settings['slides'] as $item ) {
+				echo '<dt class="elementor-repeater-item-' . esc_attr( $item['_id'] ) . esc_attr($item['btn_text_1']). '">' . $item['slide_title'] . '</dt>';
+				echo '<dd>' . $item['slide_subtitle'] . '</dd>';
+				echo '<dd>' . $item['btn_text_1'] . '</dd>';
+				echo '<dd>' . $item['btn_url_1'] . '</dd>';
+				echo '<dd>' . $item['btn_text_2'] . '</dd>';
+				echo '<dd>' . $item['btn_url_2'] . '</dd>';
 			}
 			echo '</dl>';
 		}
@@ -137,12 +171,17 @@ class Elm_Slider_Widget extends \Elementor\Widget_Base {
      * @param array $instance Saved widget settings.
      */
     protected function _content_template() {
-        // Output your widget content template here.
-		<# if ( settings.list.length ) { #>
+		?>
+    
+		<# if ( settings.slides.length ) { #>
 			<dl>
-			<# _.each( settings.list, function( item ) { #>
-				<dt class="elementor-repeater-item-{{ item._id }}">{{{ item.list_title }}}</dt>
-				<dd>{{{ item.list_content }}}</dd>
+			<# _.each( settings.slides, function( item ) { #>
+				<dt class="elementor-repeater-item-{{ item._id }}">{{{ item.slide_title }}}</dt>
+				<dd>{{{ item.slide_subtitle }}}</dd>
+				<dd>{{{ item.btn_text_1 }}}</dd>
+				<dd>{{{ item.btn_url_1 }}}</dd>
+				<dd>{{{ item.btn_text_2 }}}</dd>
+				<dd>{{{ item.btn_url_2 }}}</dd>
 			<# }); #>
 			</dl>
 		<# } #>
